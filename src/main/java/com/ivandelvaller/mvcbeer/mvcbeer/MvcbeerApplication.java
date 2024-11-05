6ac57a208bb51951d63d2d09a60ba33007608d4b
ivandelvalle <tec.ivandelvaller@outlook.com>
2024-11-04 21:54:28 -0600

    First commit

---
+++ src/main/java/com/ivandelvaller/mvcbeer/mvcbeer/MvcbeerApplication.java
@@ -0,0 +1,13 @@
+package com.ivandelvaller.mvcbeer.mvcbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcbeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcbeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivandelvaller/mvcbeer/mvcbeer/MvcbeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivandelvaller.mvcbeer.mvcbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcbeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
